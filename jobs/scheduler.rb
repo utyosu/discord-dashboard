9f3ddf243a0d62b10f8b1f20f7ffd9309082175a
utyosu <utyosu@gmail.com>
2020-03-07 08:42:05 +0900

    データが6時に更新されるように修正

--- jobs/scheduler.rb
+++ jobs/scheduler.rb
@@ -1,16 +1,16 @@
 BASE_URI = 'http://0.0.0.0:3000/'
 
-SCHEDULER.every '6h' do
+SCHEDULER.cron '0 6 * * *' do
   kings = JSON.parse(HTTP.get("#{BASE_URI}/analysises/kings").body, symbolize_names: true)
   send_event('kings', kings)
 end
 
-SCHEDULER.every '6h' do
+SCHEDULER.cron '0 6 * * *' do
   hourlyactive = JSON.parse(HTTP.get("#{BASE_URI}/analysises/hourlyactive").body, symbolize_names: true)
   send_event('hourlyactive', hourlyactive)
 end
 
-SCHEDULER.every '6h' do
+SCHEDULER.cron '0 6 * * *' do
   monthlyactive = JSON.parse(HTTP.get("#{BASE_URI}/analysises/monthlyactive").body, symbolize_names: true)
   send_event('monthlyactive', monthlyactive)
 end
